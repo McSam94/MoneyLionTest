4418889257ee5ec5717ae3aab4180451b7bbf386
McSam <mcsam1216@gmail.com>
2021-01-20 21:48:46 +0800

    add missing props in button

--- src/view/components/button/button.js
+++ src/view/components/button/button.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import cn from 'classnames';
 import './button.scss';
 
-const ButtonDiv = memo(({ onClick, className, children, ...props }) => {
+const ButtonDiv = memo(({ onClick, className, isLoading, children, ...props }) => {
     return (
         <button className={cn('ml-button', className)} onClick={onClick} {...props}>
             {isLoading ? 'Loading...' : children}
